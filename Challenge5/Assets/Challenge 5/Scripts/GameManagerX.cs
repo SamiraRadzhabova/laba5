a3655b2ccc1fbf26465c9c235c8254d0bf61e291
SamiraRadzhabova <samiraradzabova2002@gmail.com>
2023-05-10 19:40:52 +0300

    laba5

---
+++ Challenge5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timeText; //!!! 8 пункт
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton; 
+
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+
+    private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f; //  x value of the center of the left-most square
+    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+
+    private int t = 40; //!!! 8 пункт
+    
+    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
+    public void StartGame(int difficulty) // 7 пункт difficulty
+    {
+        spawnRate /= difficulty;// 7 пункт difficulty
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        StartCoroutine(UpdateTime()); //!!! 8 пункт
+        score = 0;
+        UpdateScore(0);
+        titleScreen.SetActive(false);
+    }
+
+    // While game is active spawn a random target
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+
+            if (isGameActive)
+            {
+                Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+            }
+            
+        }
+    }
+
+
+    IEnumerator UpdateTime() //!!! 8 пункт Таймер Метод
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(1);
+            t--;
+            timeText.text = "Time: " + t;
+            if (t == 0)
+            {
+                isGameActive = false;
+                if (!isGameActive)
+                {
+                    GameOver();
+                }
+            }
+
+        }
+    }
+
+    // Generate a random spawn position based on a random index from 0 to 3
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+
+    // Generates random square index from 0 to 3, which determines which square the target will appear in
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    // Update score with value from target clicked
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score; // !!! Додала + score, пункт 5
+    }
+
+    // Stop game, bring up game over text and restart button
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true); //false на true, пункт 6
+        isGameActive = false;
+    }
+
+    // Restart game by reloading the scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
